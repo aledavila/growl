04b94338c28e70161c2f827c8a46fcde1ffe9937
Samuel Ryan Waits <ryanwaits@Samuels-MacBook-Air.local>
2016-01-08 12:11:37 -0600

    load growls on index page through form

--- app.rb
+++ app.rb
@@ -1,6 +1,24 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
+enable :sessions
+
+before do
+  session[:growls] ||= []
+end
+
 get '/' do
+
+  @growls = session[:growls]
   erb :index
+
+end
+
+post '/growl' do
+
+  growl = params[:growl]
+  session[:growls].push(growl)
+
+  redirect '/'
+
 end
